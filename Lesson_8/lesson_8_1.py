b366eed77240859ea0d9a021dec74367056476bc
alexOrgnet <alex.orgnet@gmail.com>
2022-04-18 01:02:57 +0300

    homework

--- Lesson_8/lesson_8_1.py
+++ Lesson_8/lesson_8_1.py
@@ -5,13 +5,10 @@
 
 class Data:
     def __init__(self, day_month_year):
-        # self.day = day
-        # self.month = month
-        # self.year = year
         self.day_month_year = str(day_month_year)
 
     @classmethod
-    def extract(cls, day_month_year):
+    def excavate(cls, day_month_year):
         my_date = []
 
         for i in day_month_year.split():
@@ -20,28 +17,40 @@ class Data:
         return int(my_date[0]), int(my_date[1]), int(my_date[2])
 
     @staticmethod
-    def valid(day, month, year):
+    def validate(day, month, year):
+
+        error_exists = ''
 
         if 1 <= day <= 31:
-            if 1 <= month <= 12:
-                if 2019 >= year >= 0:
-                    return f'All right'
-                else:
-                    return f'Неправильный год'
-            else:
-                return f'Неправильный месяц'
+            pass
         else:
-            return f'Неправильный день'
+            error_exists += ' Неправильный день'
+
+        if 1 <= month <= 12:
+            pass
+        else:
+            error_exists += ' Неправильный месяц'
+
+        if 2022 >= year >= 0:
+            pass
+        else:
+            error_exists += ' Неправильный год'
+
+        if error_exists != '':
+            return error_exists
+        else:
+            return f'Date is in correct formatt'
+
+
 
     def __str__(self):
-        return f'Текущая дата {Data.extract(self.day_month_year)}'
+        return f'Текущая дата {Data.excavate(self.day_month_year)}'
 
 
-today = Data('11 - 1 - 2001')
+today = Data('14 - 12 - 2021')
 print(today)
-print(Data.valid(11, 11, 2022))
-print(today.valid(11, 13, 2011))
-print(Data.extract('11 - 11 - 2011'))
-print(today.extract('11 - 11 - 2020'))
-print(Data.valid(1, 11, 2000))
+print(Data.validate(24, 2, 2022))
+print(today.validate(24, 2, 2022))
+print(Data.excavate('24 - 2 - 2022'))
+print(today.excavate('24 - 2 - 2022'))
 
