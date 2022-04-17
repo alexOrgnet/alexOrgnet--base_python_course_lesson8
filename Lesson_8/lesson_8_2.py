43cbd6c97dfb9ef46dc646bb924140e9f9a3be50
alexOrgnet <alex.orgnet@gmail.com>
2022-04-18 01:29:12 +0300

    homework

---
+++ Lesson_8/lesson_8_2.py
@@ -0,0 +1,21 @@
+'''2. Создайте собственный класс-исключение, обрабатывающий ситуацию деления на ноль. Проверьте его работу на данных,
+вводимых пользователем. При вводе нуля в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.'''
+
+
+class DivideByNullError:
+    def __init__(self, denominator, divider):
+        self.denominator = denominator
+        self.divider = divider
+
+    @classmethod
+    def divide_by_null(cls, denominator, divider):
+        try:
+            return divider / denominator
+        except:
+            return (f"Деление на ноль недопустимо")
+
+
+div = DivideByNullError(100, 10)
+print(DivideByNullError.divide_by_null(0, 100))
+print(DivideByNullError.divide_by_null(1, 1000))
+print(div.divide_by_null(0, 100))
